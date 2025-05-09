8e5bbd3f8af28409469850a34963534ff83b1f82
Shreyash Hagwane <Shreyash@heaptrace.com>
2025-05-09 21:18:08 +0530

    Added public subnet

--- main.tf
+++ main.tf
@@ -17,3 +17,11 @@ resource "aws_route_table" "my_vpc_route_table" {
   
 }
 
+resource "aws_subnet" "my_subnet_public" {
+  vpc_id     = aws_vpc.my_vpc.id
+  cidr_block = "10.0.0.0/23"
+
+  tags = {
+    Name = "my_public_subnet"
+  }
+}
